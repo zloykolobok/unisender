117bd67bc0268c504993d4bd59bdd3e7179a14cf
Roman Nikolaenkov <rnikolaenkov@yandex.ru>
2018-11-12 19:56:50 +0300

    added to contacts methods: updateList and deleteList

--- src/Classes/Contacts.php
+++ src/Classes/Contacts.php
@@ -44,4 +44,45 @@ class Contacts extends Unisender
 
         return $res;
     }
+
+    /**
+     * Обновление листа рассылки
+     *
+     * @param integer $id
+     * @param string $title
+     * @param string $before_subscribe_url
+     * @param string $after_subscribe_url
+     * @return void
+     */
+    public function updateList(int $id, string $title, $before_subscribe_url, $after_subscribe_url)
+    {
+        $method = 'updateList';
+
+        $data['list_id'] = $id;
+        $data['title'] = $title;
+        $data['before_subscribe_url'] = $before_subscribe_url;
+        $data['after_subscribe_url'] = $after_subscribe_url;
+
+        $res = $this->send($data, $method);
+
+        return $res;
+    }
+
+    /**
+     * Метод для удаления списка.
+     * будут утеряны данные о дате и вермени подписки адресатов на этот список.
+     * Те адресаты, которые состояли только в этом списке, и более ни в каких других,
+     * после удаления будут отображаться в разделе «вне списков» в веб-интерфейсе.
+     *
+     * @param integer $id
+     * @return void
+     */
+    public function deleteList(int $id)
+    {
+        $method = 'deleteList';
+
+        $data['list_id'] = $id;
+
+        $res = $this->send($data, $method);
+    }
 }
