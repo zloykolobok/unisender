b56de8d956cfa18554a351685880703be1610858
Roman Nikolaenkov <rnikolaenkov@yandex.ru>
2018-11-20 13:21:25 +0300

    added controller Campaign, and added getCampaignDeliveryStats method

---
+++ src/Classes/Campaign.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * https://www.unisender.com/ru/support/api/api/#stats
+ */
+
+namespace Zloykolobok\Unisender\Classes;
+
+use Zloykolobok\Unisender\Unisender;
+
+class Campaign extends Unisender
+{
+    /**
+     * Получить отчёт по результатам доставки сообщений в заданной рассылке.
+     *
+     * @param integer $campaign_id - Идентификатор кампании
+     * @param string $changed_since - Возвращать все статусы адресов, изменившиеся начиная с указанного
+     * времени включительно (в формате «ГГГГ-ММ-ДД чч:мм:сс», часовой пояс UTC).
+     * Если аргумент отсутствует, то возвращаются все статусы.
+     * @param string $field_ids - Список id дополнительных полей, переданных через запятую.
+     * @return void
+     */
+    public function getCampaignDeliveryStats(int $campaign_id, string $changed_since = '', string $field_ids = '' )
+    {
+        $method = 'getCampaignDeliveryStats';
+        $data['campaign_id '] = $campaign_id;
+
+        if($changed_since != ''){
+            $data['changed_since'] = $changed_since;
+        }
+
+        if($field_ids != ''){
+            $data['field_ids'] = $field_ids;
+        }
+
+        $res = $this->send($data, $method);
+        return $res;
+    }
+}
